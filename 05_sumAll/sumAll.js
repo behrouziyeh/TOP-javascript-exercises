71ed56f82b414c6268a94ba9ef94fdb862e65414
behrouziyeh <204469443+behrouziyeh@users.noreply.github.com>
2025-03-23 16:20:07 +0330

    Add solutions for Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, lastNumber) {
+    let sum = 0
+    if (firstNumber >= lastNumber) {
+        startNumber = lastNumber;
+        endNumber = firstNumber;
+    } else {
+        startNumber = firstNumber;
+        endNumber = lastNumber;
+    }
+    if (isNaN(startNumber) || isNaN(endNumber) || Math.floor(startNumber) !== startNumber || Math.floor(endNumber) !== endNumber || startNumber < 0) {
+        return "ERROR";
+    }
+    for (i = startNumber; i <= endNumber; i++) {
+        sum += i;
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
