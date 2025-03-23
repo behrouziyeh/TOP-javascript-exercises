9f050341c9b9122d5745ac65326f1a720cf86c86
behrouziyeh <204469443+behrouziyeh@users.noreply.github.com>
2025-03-23 16:19:41 +0330

    Add solutions for Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...values) {
+    removedArray = arr.filter(function (x) {
+        shouldRemoved = false;
+        checkValues: for (value of values) {
+            if (x === value) {
+                shouldRemoved = true;
+                break checkValues;
+            }
+        }
+        return !shouldRemoved;
+    });
+    return removedArray;
 };
 
 // Do not edit below this line
