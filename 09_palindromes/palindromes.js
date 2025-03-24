6d9224480d79dedb70efe666dcc2e28d39969add
behrouziyeh <204469443+behrouziyeh@users.noreply.github.com>
2025-03-24 17:48:29 +0330

    Add solutions for Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const letters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanStr = str.toLowerCase().split('').filter(character => letters.includes(character)).join('');
+    const reversedCleanStr = cleanStr.split('').reverse().join('');
+    return (cleanStr === reversedCleanStr);
 };
 
 // Do not edit below this line
