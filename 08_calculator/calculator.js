3f4582a6f93283d17e4eae9d6ff1cd5f183d8793
behrouziyeh <204469443+behrouziyeh@users.noreply.github.com>
2025-03-24 17:22:58 +0330

    Add solutions for Exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce(((total, item) => total + item), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((product, item) => product * item, 1);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  return (+base) ** +power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  if (num < 0) return "ERROR";
+  else return num * factorial(num - 1)
 };
 
 // Do not edit below this line
