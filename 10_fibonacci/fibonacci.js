9effab816c24d6e944db87412e7f2faddf16fef8
behrouziyeh <204469443+behrouziyeh@users.noreply.github.com>
2025-03-24 17:54:32 +0330

    Add solutions for Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function (nth) {
+    const numberedNth = +nth;
+    if (numberedNth < 0) return "OOPS";
+    if (numberedNth === 0) return 0;
+    if (numberedNth === 1) return 1;
+    return fibonacci(numberedNth - 1) + fibonacci(numberedNth - 2);
 };
 
 // Do not edit below this line
